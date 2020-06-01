f2d75a2f9d19f6743a378b62c4fe45c4880bdf8d
root <3010113415@qq.com>
2020-06-01 16:16:02 +0800

    config

--- configedit/run_demo.py
+++ configedit/run_demo.py
@@ -2,4 +2,5 @@ import os,sys,unittest,time,shutil,webbrowser
 sys.path.append(os.path.split(os.path.realpath(__file__))[0])
 sys.path.append("abc")
 print(sys.path)
+print(os.path.split(os.path.realpath(__file__))[0])
 from config import setting
