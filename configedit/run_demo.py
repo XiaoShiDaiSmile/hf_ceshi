50b29f11cbd1ba06e7a95f981e9558a0f1676592
root <3010113415@qq.com>
2020-06-01 17:57:45 +0800

    configconfig

--- configedit/run_demo.py
+++ configedit/run_demo.py
@@ -1,6 +1,8 @@
 import os,sys
 sys.path.append(os.path.join(os.path.split(os.path.realpath(__file__))[0],'config'))
+sys.path.append(os.path.split(os.path.realpath(__file__))[0])
 sys.path.append("/opt/tomcat/apache-tomcat-8.5.55/webapps/workspace/Demo/configedit/config")
+sys.path.append("/configedit/")
 print(__file__)
 print(sys.path)
 from config import setting
