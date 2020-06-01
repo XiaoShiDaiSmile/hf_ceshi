3f89f6a89286a752fb09a4fde11aaa461ab8646c
root <3010113415@qq.com>
2020-06-01 17:12:05 +0800

    xlrd

--- configedit/run_demo.py
+++ configedit/run_demo.py
@@ -2,5 +2,5 @@ import os,sys,unittest,time,shutil,webbrowser
 sys.path.append(os.path.join(os.path.split(os.path.realpath(__file__))[0],'config'))
 print(__file__)
 print(sys.path)
-from config import setting
+from config import *
 
