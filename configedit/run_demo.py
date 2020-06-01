7ab3db8ac4c1d55c4f66c0063e91c6677bf7edf8
root <3010113415@qq.com>
2020-06-01 16:26:26 +0800

    configconfig

--- configedit/run_demo.py
+++ configedit/run_demo.py
@@ -1,5 +1,6 @@
 import os,sys,unittest,time,shutil,webbrowser
 sys.path.append(os.path.join(os.path.split(os.path.realpath(__file__))[0],'config'))
+print(__file__)
 print(sys.path)
 # from config import setting
 from package.HTMLTestRunner import HTMLTestRunner
