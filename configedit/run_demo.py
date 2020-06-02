7e0129c7070eb4256fcba3ead62eae0f9a57bb12
root <3010113415@qq.com>
2020-06-02 09:57:08 +0800

    xlrd

--- configedit/run_demo.py
+++ configedit/run_demo.py
@@ -2,6 +2,7 @@ import os,sys
 from time import sleep
 sys.path.append("\\opt\\tomcat\\apache-tomcat-8.5.55\\webapps\\workspace\\Demo\\configedit")
 sys.path.append(os.path.join(os.path.split(os.path.realpath(__file__))[0],'config'))
+sys.path.append('/configedit/config')
 print(os.path.join(os.path.split(os.path.realpath(__file__))[0],'config'))
 print(os.path.split(os.path.realpath(__file__))[0])
 print(sys.path)
