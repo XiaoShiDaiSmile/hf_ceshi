0749f6b7bdbdad62ae4c2c11df2ef328abfd332e
root <3010113415@qq.com>
2020-06-02 15:12:27 +0800

    config

--- configedit/testcases/apitest.py
+++ configedit/testcases/apitest.py
@@ -1,46 +1,11 @@
-import os,sys,unittest
-from ddt import ddt,data
-from lib.readconfig import ReadConfig
-from lib.readexcel import ReadExcel
-from config import setting
-from lib.newreport import NewReport
+import os,sys,unittest2
 
-testdata = ReadExcel(setting.SOURCE_FILE,'Sheet1').read_data()
-print(testdata)
-
-@ddt
-class Api_Test(unittest.TestCase):
-
-	def setUp(self):
-		pass
-
-	@data(*testdata)
-	def test_api1(self,data):
-		u"""nihao"""
-
-		print("test_api1")
-		try:
-			print(ReadConfig().get_config('USER','EMAIL_SERVER'))
-		except Exception as e:
-			print(str(e))
-
-		print(data['url'])
-
-	def test_api2(self):
-		try:
-			ReadConfig().set_config('USER','nihao','nihao1')
-		except Exception as e:
-			print(str(e))
-		pass
+class Api_demo(unittest2.TestCase):
 
 
-	def test_api3(self):
-		try:
-			ReadConfig().del_config('USER','nihao')
-		except Exception as e:
-			print(str(e))
-		pass
+	def test_demo1(self):
+		print("test_demo1")
 
 
 if __name__ == '__main__':
-	unittest.main()
+	unittest2.main()
