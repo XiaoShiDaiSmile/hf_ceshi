28704669a671ad7fcf7ec7c6a92b608597aeb31e
root <3010113415@qq.com>
2020-06-02 19:09:57 +0800

    configconfig

--- configedit/run_demo.py
+++ configedit/run_demo.py
@@ -6,6 +6,7 @@ from package.HTMLTestRunner import HTMLTestRunner
 
 def add_case(test_path=setting.TEST_CASE):
 	testunit=unittest2.TestSuite()
+	print("nihao")
 	discover = unittest2.loader.TestLoader().discover(test_path,pattern="**test.py",top_level_dir=None)
 	return discover
 
