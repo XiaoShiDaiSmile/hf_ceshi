c5106bd5fc514022ca517e9e3e29640cf010f7cc
root <3010113415@qq.com>
2020-06-02 09:42:59 +0800

    xlrd

--- configedit/run_demo.py
+++ configedit/run_demo.py
@@ -2,5 +2,6 @@ import os,sys
 from time import sleep
 par_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(par_dir)
-from config import setting
+# from config import setting
+import config
 
