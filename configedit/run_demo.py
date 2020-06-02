575ba0c0f049bcfb2f8c03ed7f09d00fe1f19335
root <3010113415@qq.com>
2020-06-02 17:25:31 +0800

    configconfig

--- configedit/run_demo.py
+++ configedit/run_demo.py
@@ -1,5 +1,5 @@
 #coding=UTF-8
-import os,sys,unittest2,xlrd,ConfigParser,time,webbrowser
+import io,sys,unittest2,xlrd,ConfigParser,time,webbrowser
 from config import setting
 from package.HTMLTestRunner import HTMLTestRunner
 
@@ -13,7 +13,7 @@ def add_case(test_path=setting.TEST_CASE):
 def run_case(all_case,test_report=setting.TEST_REPORT):
 	now = time.strftime('%Y-%m-%d %H-%M-%S')
 	filename = test_report+'\\'+now+'-'+'report.html'
-	fb = open(filename,'wb')
+	fb = io.open(filename,'wb')
 	runner = HTMLTestRunner(stream=fb,title="UI界面自动化测试报告",description="this is description")
 	runner.run(all_case)
 	fb.close()
