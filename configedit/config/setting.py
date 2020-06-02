f0b76e028b56d75fb0f1a6cd6f14a8a81b42a75e
root <3010113415@qq.com>
2020-06-02 16:25:20 +0800

    configconfig

--- configedit/config/setting.py
+++
@@ -1,8 +0,0 @@
-import os,sys
-
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-TEST_CASE = os.path.join(BASE_DIR,'testcases')
-TEST_REPORT = os.path.join(BASE_DIR,'report')
-TEST_CONFIG = os.path.join(BASE_DIR,'databases','config.ini')
-SOURCE_FILE = os.path.join(BASE_DIR,'databases','TestDemo.xlsx')
--- configedit/lib/newreport.py
+++
@@ -1,8 +0,0 @@
-import os,sys
-
-def NewReport(testreport):
-	lists = os.listdir(testreport)
-	lists.sort(key=lambda fn:os.path.getmtime(testreport+'\\'+fn))
-	file_new = os.path.join(testreport,lists[-1])
-	return file_new
-
