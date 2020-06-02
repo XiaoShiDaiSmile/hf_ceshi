c594527bd69ba313b8da643af9b35058e2eb3de5
root <3010113415@qq.com>
2020-06-02 11:12:56 +0800

    config

--- configedit/run_demo.py
+++ configedit/run_demo.py
@@ -1,4 +1,31 @@
 import os,sys,unittest,xlrd,ConfigParser
 from config import setting
 from lib.readconfig import ReadConfig
+from package.HTMLTestRunner import HTMLTestRunner
+
+
+def add_case(test_path=setting.TEST_CASE):
+	discover = unittest.defaultTestLoader.discover(test_path,pattern="**test.py")
+	return discover
+
+
+def run_case(all_case,test_report=setting.TEST_REPORT):
+	now = time.strftime('%Y-%m-%d %H-%M-%S')
+	filename = test_report+'\\'+now+'-'+'report.html'
+	fb = open(filename,'wb')
+	runner = HTMLTestRunner(stream=fb,
+						    title="UI界面自动化测试报告",
+						    description="this is description"
+						   )
+	runner.run(all_case)
+	#webbrowser.open_new_tab(filename)
+	fb.close()
+	# report = new_report(setting.TEST_REPORT)
+	# print(report)
+	# send_email(report)
+
+
+if __name__ == '__main__':
+	cases = add_case()
+	run_case(cases)
 
